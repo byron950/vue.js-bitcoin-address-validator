a8fa86ffe51a58bfe53938a2e085b5e62f2c48e1
Byron <105740608+byron950@users.noreply.github.com>
2023-11-30 16:44:47 +0200

    Added error message functionality to CheckAddress

--- src/components/functionality/CheckAddress.vue
+++ src/components/functionality/CheckAddress.vue
@@ -1,4 +1,20 @@
 <template>
+  <the-error-message
+    v-if="inputIsBlank"
+    title="Blank Input"
+    @close="confirmError"
+  >
+    <template #default>
+      <p>Input is invald</p>
+      <p>
+        Please check the address input field and make sure that you have
+        supplied an address, blank inputs are not accepted.
+      </p>
+    </template>
+    <template #actions>
+      <the-button @click="confirmError" buttonText="OK"></the-button>
+    </template>
+  </the-error-message>
   <address-box></address-box>
   <the-output-field>
     <div v-if="addressFinal === 'valid'">
@@ -7,39 +23,51 @@
     <div v-else-if="addressFinal === 'invalid'">
       <p>address is invalid</p>
     </div>
-    <div v-else>
-    </div>
+    <div v-else></div>
   </the-output-field>
 </template>
 
 <script>
 import TheOutputField from "../layout/TheOutputField.vue";
+import TheErrorMessage from "../ui/TheErrorMessage.vue";
 
 export default {
   data() {
     return {
-      addressFinal: String
+      addressFinal: String,
+      inputIsBlank: false,
     };
   },
-  components: { TheOutputField },
+  components: { TheOutputField, TheErrorMessage },
   methods: {
     checkAddress(address) {
+      if(
+        address.trim() === ''
+      ) {
+        this.inputIsBlank = true;
+        return;
+      }
       const mempoolUrl = `https://mempool.space/api/address/${address}`;
-      fetch(mempoolUrl).then(response => response.json()).then(data => {
-        console.log(data);
-        this.addressFinal = 'valid';
-        return address;
-      })
-      .catch(error => {
-        console.error("Error fetching data: ", error);
-        this.addressFinal = 'invalid';
-      })
+      fetch(mempoolUrl)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          this.addressFinal = "valid";
+          return address;
+        })
+        .catch((error) => {
+          console.error("Error fetching data: ", error);
+          this.addressFinal = "invalid";
+        });
     },
+    confirmError() {
+      this.inputIsBlank = false;
+    }
   },
   provide() {
-      return {
-        checkAddress: this.checkAddress,
-      };
-    },
+    return {
+      checkAddress: this.checkAddress,
+    };
+  },
 };
 </script> 
